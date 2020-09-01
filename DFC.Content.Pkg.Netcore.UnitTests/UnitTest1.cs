31a5d41d8534b71d9690f9b5fdb6775218773cb0
mofais <raymar786@gmail.com>
2020-09-01 11:38:27 +0100

    added solutionbasename matching repo name

---
+++ DFC.Content.Pkg.Netcore.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DFC.Content.Pkg.Netcore.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ DFC.Content.Pkg.Netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DFC.Content.Pkg.Netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
