5ed70de611114d3e8b4b1f61a45ea6bcc0faf89f
snipered2003 <snipered@hotmail.com>
2020-09-18 12:28:05 +0100

    removed tests into one project (#9)
    * removed tests into one project
    
    * missing guid

---
+++ dfc-content-pkg-netcore-unitTests/ApiProcessorService/FakeHttpHandlers/IFakeHttpRequestSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace DFC.Content.Pkg.Netcore.ApiProcessorService.UnitTests.FakeHttpHandlers
+{
+    public interface IFakeHttpRequestSender
+    {
+        HttpResponseMessage Send(HttpRequestMessage request);
+    }
+}
